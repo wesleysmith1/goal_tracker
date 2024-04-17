88c33688a2f388bb27d6c1e70947110debff1a68
Ubuntu <ubuntu@ip-172-31-8-143.us-west-1.compute.internal>
2024-04-17 22:45:04 +0000

    dumb ass chart

--- goal_tracker/react_app/src/pages/Goals.js
+++ goal_tracker/react_app/src/pages/Goals.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import GoalComponent from '../components/GoalComponent'; // Adjust the path as necessary
+import SimpleLineChart from '../components/SimpleLineChart'; 
 
 function Goals() {
   return (
     <div>
       <h2>Your Goals</h2>
       <GoalComponent />
+      <SimpleLineChart />
     </div>
   );
 }
